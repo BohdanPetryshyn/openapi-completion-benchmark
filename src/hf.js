08ff88d1653ace602ad3f19c214ea11421961308
Bohdan Petryshyn <b.y.petryshyn@gmail.com>
2024-04-21 16:44:59 +0300

    feat: evaluate fine-tuned models

--- src/hf.js
+++ src/hf.js
@@ -8,6 +8,10 @@ const endpoints = {
     "https://ce5wjdvaod3s7uq6.eu-west-1.aws.endpoints.huggingface.cloud",
   "codellama/CodeLlama-13b-hf":
     "https://d5ht9s3sseewr2p1.us-east-1.aws.endpoints.huggingface.cloud",
+  "BohdanPetryshyn/codellama-7b-openapi-completion-merged":
+    "https://io15kbfmkmya9cyu.us-east-1.aws.endpoints.huggingface.cloud",
+  "BohdanPetryshyn/codellama-7b-clone":
+    "https://z6rp06xawwzv2n7u.us-east-1.aws.endpoints.huggingface.cloud",
 };
 
 let hfClient = new HfInference(process.env.HF_API_KEY);
